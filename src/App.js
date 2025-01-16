3bc9baea8d6150404aa361a205430ac7145d499c
Phakkhanan09 <Phakkhanan.nak@gmail.com>
2025-01-16 10:04:12 +0700

    Event Class Component
    Event Class Component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import { Header,Content,Footer } from './func-components';
-import Calender from './class-components';
+import Button from './event-class';
 
 function App() {
-  return(
-    <>
-      <Header/>
-      <p><center><Calender/></center></p>
-      <Content/>
-      <Footer/>
-    </>
-  )
+  return <Button/>
+    
+  
 }
 
 export default App;
