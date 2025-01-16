6bd33214a3e6aa2b491cd1be18d74bbf2eae996a
Phakkhanan09 <Phakkhanan.nak@gmail.com>
2025-01-16 11:36:44 +0700

    Creat event data
    Creat event data and cal alitlrbit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Calculator} from './calculator';
+import {EventData1} from './event-data';
 
 export default function App(){
-  return<Calculator/>
+  return<EventData1/>
 }
